d71076dae174930e66a571c1f7402414d62e0dd4
Bojan Endrovski <bojan.endrovski@gmail.com>
2024-12-04 16:54:59 +0100

    🪺 nested structs

--- visit-struct-test.cpp
+++ visit-struct-test.cpp
@@ -2,10 +2,97 @@
 //
 
 #include <iostream>
+#
+
+#include "visit_struct.hpp"
+#include "visit_struct_intrusive.hpp"
+
+struct Vec3
+{
+	float x, y, z;
+};
+
+struct Z
+{
+	int z0;
+	float z1;
+	std::string z2;
+};
+
+struct A
+{
+	int m;
+	float n;
+	Z o;
+};
+
+struct B
+{
+	int a;
+	float b;
+	std::string c;
+	Vec3 d;
+	A e;
+};
+
+VISITABLE_STRUCT(Vec3, x, y, z);
+VISITABLE_STRUCT(Z, z0, z1, z2);
+VISITABLE_STRUCT(A, m, n, o);
+VISITABLE_STRUCT(B, a, b, c, d, e);
+
+// template <typename T, std::enable_if_t(!std::is_base_of<visit_struct::visitable_base, T>::value = 0)
+
+struct printer {
+
+	printer
+
+
+	void operator()(const char* field, Vec3 vec)
+	{
+		std::cout << field << " = (" << vec.x << ", " << vec.y << ", " << vec.z << ")" << std::endl;
+	}
+
+	// Primitive types
+	template <typename T, std::enable_if_t<std::is_arithmetic<T>::value>* = nullptr>
+	void operator()(const char* field, const T& value)
+	{
+		std::cout << field << " = " << value << std::endl;
+	}
+
+	// Strings
+	void operator()(const char* field, const std::string& value)
+	{
+		std::cout << field << " = \"" << value << "\"" << std::endl;
+	}
+	
+	// Structs
+	template <typename T, typename = std::enable_if_t<visit_struct::traits::is_visitable<T>::value>>
+	void operator()(const char* field, const T& value)
+	{
+		std::cout << field << " = {" << std::endl;
+		visit_struct::for_each(value, *this);
+		std::cout << "}" << std::endl;
+	}
+
+	/*
+	template <typename T, typename = std::enable_if_t<visit_struct::traits::is_visitable<T>>::vallue = 0>
+	void operator()(const char* field, const T& value)
+	{
+		printer p;
+		visit_struct::for_each(value, p);
+	}
+	*/
+
+	int depth = 0;
+};
+
+ 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	B b{ 1, 2.0f, "hello", {3.0f, 4.0f, 5.0f}, {6, 7.0f, {8, 9.0f, "world"}} };
+	printer p;
+	visit_struct::for_each(b, p);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
