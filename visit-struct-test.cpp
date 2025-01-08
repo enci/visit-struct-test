4aab61d29b611c0c69030205a4bc59ec987853a5
Bojan Endrovski <bojan.endrovski@gmail.com>
2025-01-08 13:50:01 +0100

    ➡️ now with vectors

--- visit-struct-test.cpp
+++ visit-struct-test.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #include "visit_struct.hpp"
 #include "visit_struct_intrusive.hpp"
@@ -25,6 +26,7 @@ struct A
 	int m;
 	float n;
 	Z o;
+	std::vector<int> v;
 };
 
 struct B
@@ -38,7 +40,7 @@ struct B
 
 VISITABLE_STRUCT(Vec3, x, y, z);
 VISITABLE_STRUCT(Z, z0, z1, z2);
-VISITABLE_STRUCT(A, m, n, o);
+VISITABLE_STRUCT(A, m, n, o, v);
 VISITABLE_STRUCT(B, a, b, c, d, e);
 
 struct printer {
@@ -62,6 +64,34 @@ struct printer {
 		std::cout << field << " = \"" << value << "\"" << std::endl;
 	}
 	
+
+	// Vectors of primitive types
+	template <typename T, std::enable_if_t<std::is_arithmetic<T>::value>* = nullptr>
+	void operator()(const char* field, const std::vector<T>& value)
+	{
+		for (int i = 0; i < depth; i++) std::cout << "  ";
+		std::cout << field << " = [";
+
+		for (const auto& v : value)
+			std::cout << v << ",";
+
+		std::cout << "]" << std::endl;
+	}
+
+	// Vectors of structs
+	template <typename T, typename = std::enable_if_t<visit_struct::traits::is_visitable<T>::value>>
+	void operator()(const char* field, const std::vector<T>& value)
+	{
+		for (int i = 0; i < depth; i++) std::cout << "  ";
+		std::cout << field << " = [" << std::endl;
+		depth++;
+		for (const auto& v : value)
+			visit_struct::for_each(v, *this);
+		depth--;
+		for (int i = 0; i < depth; i++) std::cout << "  ";
+		std::cout << "]" << std::endl;
+	}		
+	
 	// Structs
 	template <typename T, typename = std::enable_if_t<visit_struct::traits::is_visitable<T>::value>>
 	void operator()(const char* field, const T& value)
@@ -78,7 +108,8 @@ struct printer {
 	int depth = 0;
 };
 
-struct to_json {
+struct to_json
+{
 	to_json() : json(nlohmann::json::object()) {}
 
 	void operator()(const char* field, Vec3 vec)
@@ -99,6 +130,27 @@ struct to_json {
 		json[field] = value;
 	}
 
+	// Vectors of primitive types
+	template <typename T, std::enable_if_t<std::is_arithmetic<T>::value>* = nullptr>
+	void operator()(const char* field, const std::vector<T>& value)
+	{
+		json[field] = value;
+	}
+
+	// Vectors of structs
+	template <typename T, typename = std::enable_if_t<visit_struct::traits::is_visitable<T>::value>>
+	void operator()(const char* field, const std::vector<T>& value)
+	{
+		std::vector<nlohmann::json> j;
+		for (const auto& v : value)
+		{
+			to_json tj;
+			visit_struct::for_each(v, tj);
+			j.push_back(tj.json);
+		}
+		json[field] = j;
+	}
+
 	// Structs
 	template <typename T, typename = std::enable_if_t<visit_struct::traits::is_visitable<T>::value>>
 	void operator()(const char* field, const T& value)
@@ -111,9 +163,66 @@ struct to_json {
 	nlohmann::json json;
 };
 
+struct from_json
+{
+	from_json(const nlohmann::json& json) : json(json) {}
+
+	void operator()(const char* field, Vec3& vec)
+	{
+		vec.x = json[field][0];
+		vec.y = json[field][1];
+		vec.z = json[field][2];
+	}
+
+	// Primitive types
+	template <typename T, std::enable_if_t<std::is_arithmetic<T>::value>* = nullptr>
+	void operator()(const char* field, T& value)
+	{
+		value = json[field];
+	}
+
+	// Strings
+	void operator()(const char* field, std::string& value)
+	{
+		value = json[field];
+	}
+
+	// Vectors of primitive types
+	template <typename T, std::enable_if_t<std::is_arithmetic<T>::value>* = nullptr>
+	void operator()(const char* field, std::vector<T>& value)
+	{
+		value = json[field].get<std::vector<T>>();
+	}
+
+	// Vectors of structs
+	template <typename T, typename = std::enable_if_t<visit_struct::traits::is_visitable<T>::value>>
+	void operator()(const char* field, std::vector<T>& value)
+	{
+		for (const auto& j : json[field])
+		{
+			T t;
+			from_json fj(j);
+			visit_struct::for_each(t, fj);
+			value.push_back(t);
+		}
+	}
+
+	// Structs
+	template <typename T, typename = std::enable_if_t<visit_struct::traits::is_visitable<T>::value>>
+	void operator()(const char* field, T& value)
+	{
+		from_json fj(json[field]);
+		visit_struct::for_each(value, fj);
+	}
+
+private:
+	const nlohmann::json& json;
+};
+
 int main()
 {
-	B b{ 1, 2.0f, "hello", {3.0f, 4.0f, 5.0f}, {6, 7.0f, {8, 9.0f, "world"}} };
+	std::cout << "=========================================================" << std::endl;
+	B b{ 1, 2.0f, "hello", {3.0f, 4.0f, 5.0f}, {6, 7.0f, {8, 9.0f, "world"}, { -1, -2, -3 }} };
 	printer p;
 	visit_struct::for_each(b, p);
 
@@ -123,6 +232,16 @@ int main()
 	std::ofstream out("output.json");
 	out << tj.json.dump(4) << std::endl;
 
+	std::cout << "=========================================================" << std::endl;
+	nlohmann::json json;
+	std::ifstream in("output.json");
+	in >> json;
 
-	// std::cout << tj.json.dump(4) << std::endl;
+	B b2;
+	from_json fj(json);
+	visit_struct::for_each(b2, fj);
+
+
+	printer p2;
+	visit_struct::for_each(b2, p2);
 }
